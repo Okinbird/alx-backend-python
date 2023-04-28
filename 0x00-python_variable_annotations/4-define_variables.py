f1c204333f60128cd2027ea4319d897b08aa50c8
Okinbird <okinyinka@yahoo.co.uk>
2023-04-28 10:04:05 -0700

    Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Description: Define and annotate the following variables with the
+    specified values
+    Arguments:  a - an integer with value 1
+                pi - a float with value 3.14
+                i_understand_annotations - a boolean with value True
+                school - str with value 'Holberton'
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
