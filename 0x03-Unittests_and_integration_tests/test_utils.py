f12bdab6dfb1a00a743003c0dd7af9ce5574c2f8
Okinbird <okinyinka@yahoo.co.uk>
2023-05-21 14:52:12 -0700

    Parameterize a unit test

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Test SUITE Unittest module Task """
+
+import requests
+from unittest import TestCase, mock
+from unittest.mock import patch, Mock
+from parameterized import parameterized, parameterized_class
+from utils import access_nested_map, get_json, memoize
+
+
+class TestAccessNestedMap(TestCase):
+    """ Class for testing Nested Map function """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, map, path, expected_output):
+        """ Test method return output """
+        real_output = access_nested_map(map, path)
+        self.assertEqual(real_output, expected_output)
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, map, path, wrong_output):
+        """ Test method raises correct exception """
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(map, path)
+            self.assertEqual(wrong_output, error.exception)
